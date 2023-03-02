d6f64e61ba422514fba069a8db0f9b434b1e3308
Barcollin Jay <jubilates@hotmail.com>
2023-03-02 10:14:41 +0000

    added the run fun of type hub with few select cases

--- pkg/chat/hub.go
+++ pkg/chat/hub.go
@@ -6,3 +6,29 @@ type Hub struct {
 	register   chan *Client
 	unregister chan *Client
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+
+				delete(h.clients, client)
+				close(client.Send)
+			}
+		}
+	}
+
+}
